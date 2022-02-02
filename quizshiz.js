7d47ed1761d388c95e4d78401dd246a7b82d23ee
CGrizzy <chrisgraner1@gmail.com>
2022-02-02 09:56:34 -0800

    punishment for transgression

--- quizshiz.js
+++ quizshiz.js
@@ -2,17 +2,28 @@ $(document).ready(function(){
 
   for (let n = 1; n<= 5; n++){
   $("#c"+n).click(function(){
-  alert("Correct!");
-  $("#q"+n).text("Correct Answer!!!")
+
+  $("#aN"+n).text("Correct Answer!!!")
   $("")
   });
 
   $(".false"+n).click(function(){
-  alert("False!");
-  $("#q"+n).text("Incorrect, try again!")
+
+  $("#aN"+n).text("Incorrect, try again!")
+
+for (let x = 1; x<= 5000; x = x+100){
+  setTimeout(function() {
+    alert("You will now pay for your transgression. ");
+  }, x * 1000);
+}
+
   });
+
+
+
   }
-  
+
+
   $()
 
 });
