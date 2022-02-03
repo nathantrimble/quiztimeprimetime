32451e6b98b2d5253801603e0462cd898aae8f55
Nate <nathantrimble1@gmail.com>
2022-02-03 10:21:07 -0800

    big boy

--- quizshiz.js
+++ quizshiz.js
@@ -2,28 +2,17 @@ $(document).ready(function(){
 
   for (let n = 1; n<= 5; n++){
   $("#c"+n).click(function(){
-
   $("#aN"+n).text("Correct Answer!!!")
-  $("x")
   });
 
   $(".false"+n).click(function(){
-
   $("#aN"+n).text("Incorrect, try again!")
-
-for (let x = 1; x<= 5000; x = x+100){
-  setTimeout(function() {
     alert("You will now pay for your transgression. ");
-  }, x * 1000);
-}
+    alert("You will now pay for your transgression. ");
+    alert("You will now pay for your transgression. ");
+    alert("You will now pay for your transgression. ");
+    alert("You will now pay for your transgression. ");
 
   });
-
-
-
   }
-
-
-  $()
-
 });
